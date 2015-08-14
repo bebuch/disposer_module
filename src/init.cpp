8bc86f6fae229902d671eada17a9231bb0e791c7
Benjamin Roland Buch <benni.buch@gmail.com>
2015-08-14 21:40:52 +0200

    boost.process removed

--- src/init.cpp
+++ src/init.cpp
@@ -11,7 +11,6 @@
 #include "big_loader.hpp"
 #include "big_saver.hpp"
 #include "raster.hpp"
-#include "async_program.hpp"
 #include "async_get.hpp"
 #include "add_to_log.hpp"
 #include "subbitmap.hpp"
@@ -24,7 +23,6 @@ namespace disposer_module{
 		big_loader::init();
 		big_saver::init();
 		raster::init();
-		async_program::init();
 		async_get::init();
 		add_to_log::init();
 		subbitmap::init();

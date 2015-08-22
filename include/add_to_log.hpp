0fe071e6a682ba95a8568eddc6c28f9276e9f9b3
Benjamin Roland Buch <benni.buch@gmail.com>
2015-08-23 01:55:19 +0200

    new multiple include protection

--- include/add_to_log.hpp
+++ include/add_to_log.hpp
@@ -6,8 +6,8 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
 //-----------------------------------------------------------------------------
-#ifndef _disposer_module_add_to_log_hpp_INCLUDED_
-#define _disposer_module_add_to_log_hpp_INCLUDED_
+#ifndef _disposer_module__add_to_log_hpp_INCLUDED_
+#define _disposer_module__add_to_log_hpp_INCLUDED_
 
 #include <disposer/disposer.hpp>
 
--- include/mask_non_print.hpp
+++ include/mask_non_print.hpp
@@ -6,8 +6,8 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
 //-----------------------------------------------------------------------------
-#ifndef _disposer_module_mask_non_print_hpp_INCLUDED_
-#define _disposer_module_mask_non_print_hpp_INCLUDED_
+#ifndef _disposer_module__mask_non_print_hpp_INCLUDED_
+#define _disposer_module__mask_non_print_hpp_INCLUDED_
 
 #include <string>
 

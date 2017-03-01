fac2f00fe3e12c5ef97582f83508dd0a5e7b83af
Benjamin Roland Buch <benni.buch@gmail.com>
2017-03-01 12:47:21 +0100

    use fold expressions

--- include/make_string.hpp
+++ include/make_string.hpp
@@ -16,73 +16,32 @@
 namespace disposer_module{
 
 
-	namespace impl{ namespace make_string{
-
-
-		template < typename Stream >
-		inline void stream_output(Stream&){}
-
-		template < typename Stream, typename Head >
-		inline void stream_output(Stream& s, Head&& head){
-			s << std::forward< Head >(head);
-		}
-
-		template < typename Stream, typename Head, typename ... T >
-		inline void stream_output(Stream& s, Head&& head, T&& ... args){
-			s << std::forward< Head >(head);
-			stream_output(s, std::forward< T >(args) ...);
-		}
-
-		template < typename Stream, typename Separator >
-		inline void stream_output_separated_by(Stream&, Separator const&){}
-
-		template < typename Stream, typename Separator, typename Head >
-		inline void stream_output_separated_by(
-			Stream& s,
-			Separator const&,
-			Head&& head
-		){
-			s << std::forward< Head >(head);
-		}
-
-		template <
-			typename Stream, typename Separator, typename Head, typename ... T
-		>
-		inline void stream_output_separated_by(
-			Stream& s,
-			Separator const& separator,
-			Head&& head,
-			T&& ... args
-		){
-			s << std::forward< Head >(head) << separator;
-			stream_output_separated_by(
-				s, separator, std::forward< T >(args) ...
-			);
-		}
-
-
-	} }
-
-
 	template < typename ... T >
 	inline std::string make_string(T&& ... args){
 		std::ostringstream os;
 		os << std::boolalpha;
-		impl::make_string::stream_output(os, std::forward< T >(args) ...);
+		(os << ... << static_cast< T&& >(args));
 		return os.str();
 	}
 
-	template < typename Separator, typename ... T >
+	template < typename Separator, typename T, typename ... Ts >
 	inline std::string make_string_separated_by(
 		Separator const& separator,
-		T&& ... args
+		T&& arg,
+		Ts&& ... args
 	){
 		std::ostringstream os;
 		os << std::boolalpha;
-		impl::make_string::stream_output_separated_by(
-			os, separator, std::forward< T >(args) ...
-		);
+		os << static_cast< T&& >(arg);
+		for(auto&& arg: { static_cast< Ts&& >(args) ... }){
+			os << separator << static_cast< T&& >(arg);
+		}
 		return os.str();
+	}
+
+	template < typename Separator >
+	inline std::string make_string_separated_by(Separator const&){
+		return {};
 	}
 
 

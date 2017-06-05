ba971d34405c162f046be28a3296525840f43e99
Benjamin Buch <benni.buch@gmail.com>
2017-06-05 23:34:38 +0200

    simplifications

--- src/save.cpp
+++ src/save.cpp
@@ -22,18 +22,14 @@ namespace disposer_module::save{
 	using namespace disposer::literals;
 	namespace hana = boost::hana;
 	using namespace hana::literals;
-	using namespace std::literals::string_literals;
+	using hana::type_c;
 
 
 	using t1 = std::string;
 	using t2 = std::vector< std::string >;
 	using t3 = std::vector< std::vector< std::string > >;
 
-	constexpr auto types = hana::tuple_t<
-			t1,
-			t2,
-			t3
-		>;
+	constexpr auto types = hana::tuple_t< t1, t2, t3 >;
 
 	using ng1 = name_generator< std::size_t >;
 	using ng2 = name_generator< std::size_t, std::size_t >;
@@ -97,64 +93,56 @@ namespace disposer_module::save{
 		auto init = make_register_fn(
 			configure(
 				"content"_in(types, required),
-				"id_digits"_param(hana::type_c< std::size_t >,
+				"id_digits"_param(type_c< std::size_t >,
 					default_values(std::size_t(4))),
-				"i_digits"_param(hana::type_c< std::size_t >,
+				"i_digits"_param(type_c< std::size_t >,
 					default_values(std::size_t(2))),
-				"j_digits"_param(hana::type_c< std::size_t >,
+				"j_digits"_param(type_c< std::size_t >,
 					default_values(std::size_t(2))),
 				"name"_param(name_generator_types,
-					enable(
-						[](auto const& iop, auto type){
-							if constexpr(type == hana::type_c< ng1 >){
-								return iop("content"_in).is_enabled(
-									hana::type_c< std::string >);
-							}else if constexpr(type == hana::type_c< ng2 >){
-								return iop("content"_in).is_enabled(
-									hana::type_c< std::vector< std::string > >);
-							}else{
-								return iop("content"_in).is_enabled(
-									hana::type_c< std::vector<
-										std::vector< std::string > > >
-								);
-							}
-						}),
-					parser(
-						[](auto const& iop, std::string_view data, auto type){
-							if constexpr(type == hana::type_c< ng1 >){
-								return make_name_generator(
-									data,
-									std::make_pair("id",
-										format{iop("id_digits"_param).get()})
-								);
-							}else if constexpr(type == hana::type_c< ng2 >){
-								return make_name_generator(
-									data,
-									std::make_pair("id",
-										format{iop("id_digits"_param).get()}),
-									std::make_pair("i",
-										format{iop("i_digits"_param).get()})
-								);
-							}else{
-								return make_name_generator(
-									data,
-									std::make_pair("id",
-										format{iop("id_digits"_param).get()}),
-									std::make_pair("i",
-										format{iop("i_digits"_param).get()}),
-									std::make_pair("j",
-										format{iop("j_digits"_param).get()})
-								);
-							}
-						}),
+					enable([](auto const& iop, auto type){
+						auto const& out = iop("content"_in);
+						if constexpr(type == type_c< ng1 >){
+							return out.is_enabled(type_c< t1 >);
+						}else if constexpr(type == type_c< ng2 >){
+							return out.is_enabled(type_c< t2 >);
+						}else{
+							return out.is_enabled(type_c< t3 >);
+						}
+					}),
+					parser([](
+						auto const& iop, std::string_view data, auto type
+					){
+						if constexpr(type == type_c< ng1 >){
+							return make_name_generator(
+								data,
+								std::make_pair("id",
+									format{iop("id_digits"_param).get()})
+							);
+						}else if constexpr(type == type_c< ng2 >){
+							return make_name_generator(
+								data,
+								std::make_pair("id",
+									format{iop("id_digits"_param).get()}),
+								std::make_pair("i",
+									format{iop("i_digits"_param).get()})
+							);
+						}else{
+							return make_name_generator(
+								data,
+								std::make_pair("id",
+									format{iop("id_digits"_param).get()}),
+								std::make_pair("i",
+									format{iop("i_digits"_param).get()}),
+								std::make_pair("j",
+									format{iop("j_digits"_param).get()})
+							);
+						}
+					}),
 					type_as_text(
-						hana::make_pair(hana::type_c< name_generator<
-							std::size_t > >, "file"_s),
-						hana::make_pair(hana::type_c< name_generator<
-							std::size_t, std::size_t > >, "file_list"_s),
-						hana::make_pair(hana::type_c< name_generator<
-							std::size_t, std::size_t, std::size_t > >,
-								"file_list_list"_s)
+						hana::make_pair(type_c< ng1 >, "file"_s),
+						hana::make_pair(type_c< ng2 >, "file_list"_s),
+						hana::make_pair(type_c< ng3 >, "file_list_list"_s)
 					)
 				)
 			),

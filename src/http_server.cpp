a76407943feac464118cdd860ef528406007b620
Benjamin Roland Buch <benni.buch@gmail.com>
2017-06-16 17:49:40 +0200

    use new component module syntax

--- src/http_server.cpp
+++ src/http_server.cpp
@@ -402,26 +402,30 @@ namespace disposer_module::http_server_component{
 					component("thread_count"_param).get());
 			},
 			component_modules(
-				"websocket"_module(
-					module_configure(
-						"data"_in(hana::type_c< std::string >, required),
-						"service_name"_param(hana::type_c< std::string >)
-					),
-					normal_id_increase(),
-					[](auto& component, auto const& module){
-						auto init = component.data()
-							.init(module("service_name"_param).get());
-						auto key = init.key;
-						return [&component, key]
-							(auto& module, std::size_t /*id*/){
-								auto list = module("data"_in).get_references();
-								if(list.empty()) return;
-								auto iter = list.end();
-								--iter;
-								component.data().send(key, iter->second.get());
-							};
-					}
-				)
+				"websocket"_module([](auto& component){
+					return make_module_register_fn(
+						module_configure(
+							"data"_in(hana::type_c< std::string >, required),
+							"service_name"_param(hana::type_c< std::string >)
+						),
+						normal_id_increase(),
+						[&component](auto const& module){
+							auto init = component.data()
+								.init(module("service_name"_param).get());
+							auto key = init.key;
+							return [&component, key]
+								(auto& module, std::size_t /*id*/){
+									auto list =
+										module("data"_in).get_references();
+									if(list.empty()) return;
+									auto iter = list.end();
+									--iter;
+									component.data().send(key,
+										iter->second.get());
+								};
+						}
+					);
+				})
 			)
 		);
 

e378cb2ce78fedae50ba6e76e8dc71cd4ba4bfa9
Benjamin Roland Buch <benni.buch@gmail.com>
2017-07-25 13:55:32 +0200

    removed unused header

--- src/subbitmap.cpp
+++ src/subbitmap.cpp
@@ -6,8 +6,6 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
 //-----------------------------------------------------------------------------
-#include "thread_pool.hpp"
-
 #include <disposer/module.hpp>
 
 #include <bitmap/subbitmap.hpp>

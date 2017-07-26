c3324ff27f3a141bb300205d6aae4bb475eb08f3
Benjamin Roland Buch <benni.buch@gmail.com>
2017-07-26 17:01:34 +0200

    use histogram function from bitmap library

--- src/histogram.cpp
+++ src/histogram.cpp
@@ -6,9 +6,9 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
 //-----------------------------------------------------------------------------
-#include <disposer/module.hpp>
+#include <bitmap/histogram.hpp>
 
-#include <bitmap/bitmap.hpp>
+#include <disposer/module.hpp>
 
 #include <boost/dll.hpp>
 
@@ -38,106 +38,17 @@ namespace disposer_module::histogram{
 		>;
 
 
-	template < typename T, typename IndexFn >
-	void histogram_count(
-		std::vector< std::size_t >& histogram,
-		bitmap< T > const& image,
-		IndexFn const& calc_index
-	){
-		for(auto v: image){
-			if constexpr(std::is_floating_point_v< T >){
-				if(std::isnan(v)) continue;
-			}
-
-			++histogram[calc_index(v)];
-		}
-	}
-
-
-	template < typename T >
-	struct counter{
-		std::vector< std::size_t >& histogram;
-		bitmap< T > const& image;
-		T const min;
-		T const max;
-
-		template < typename IndexFn >
-		void operator()(IndexFn const& calc_index){
-			auto const min_check_fn = [min = min](auto const& fn){
-					return [&fn, min](auto v){ return fn(std::max(v, min)); };
-				};
-			auto const max_check_fn = [max = max](auto const& fn){
-					return [&fn, max](auto v){ return fn(std::min(v, max)); };
-				};
-			auto const minmax_check_fn =
-				[&min_check_fn, &max_check_fn](auto const& fn){
-					return max_check_fn(min_check_fn(fn));
-				};
-
-			bool need_min_check = std::is_floating_point_v< T > ? true
-				: std::numeric_limits< T >::min() < min;
-			bool need_max_check = std::is_floating_point_v< T > ? true
-				: std::numeric_limits< T >::max() > max;
-
-			if(need_min_check && need_max_check){
-				histogram_count(histogram, image, minmax_check_fn(calc_index));
-			}else if(need_max_check){
-				histogram_count(histogram, image, max_check_fn(calc_index));
-			}else if(need_min_check){
-				histogram_count(histogram, image, min_check_fn(calc_index));
-			}else{
-				histogram_count(histogram, image, calc_index);
-			}
-		}
-	};
-
-
-	template < typename T, bool = std::is_integral_v< T > >
-	struct make_diff_type{ using type = T; };
-
-	template < typename T >
-	struct make_diff_type< T, true >{ using type = std::make_unsigned_t< T >; };
-
-	template < typename T >
-	using make_diff_type_t = typename make_diff_type< T >::type;
-
-
 	template < typename Module, typename T >
 	std::vector< std::size_t > exec(
 		Module const& module,
 		bitmap< T > const& image
 	){
-		auto const min = module("min"_param).get(hana::type_c< T >);
-		auto const max = module("max"_param).get(hana::type_c< T >);
-
-		// for signed integer types diff may not fit in singed range but it
-		// fits in ever in the corresponding unsigned range
-		using diff_type = make_diff_type_t< T >;
-
-		auto const diff = static_cast< diff_type >(max - min);
-		auto const bin_count = module("bin_count"_param).get();
-		auto const max_index = bin_count - 1;
-
-		std::vector< std::size_t > result(bin_count);
-		counter< T > calc{result, image, min, max};
-		if(min == 0 && diff == max_index){
-			calc([](auto v){ return static_cast< std::size_t >(v); });
-		}else{
-			calc([min, max_index, diff](auto v){
-					auto const v0 = static_cast< diff_type >(v - min);
-					return static_cast< std::size_t >(v0 * max_index / diff);
-				});
-		}
-
-		if(module("cumulative"_param).get()){
-			std::size_t sum = 0;
-			for(auto& v: result){
-				sum += v;
-				v = sum;
-			}
-		}
-
-		return result;
+		return bmp::histogram(image,
+			module("min"_param).get(hana::type_c< T >),
+			module("max"_param).get(hana::type_c< T >),
+			module("bin_count"_param).get(),
+			module("cumulative"_param).get()
+		);
 	}
 
 

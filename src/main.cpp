fa598fddedfd85215cf33006555d70521c44f49d
Benjamin Roland Buch <benni.buch@gmail.com>
2017-08-01 14:37:18 +0200

    fix log include

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
 //-----------------------------------------------------------------------------
+#include <logsys/log.hpp>
 #include <logsys/stdlogb.hpp>
 
 #include <disposer/disposer.hpp>

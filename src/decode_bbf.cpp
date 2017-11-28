4a2a088e33c430b7973d6b4edcf0bce9f4675814
Benjamin Roland Buch <benni.buch@gmail.com>
2017-11-28 17:45:22 +0100

    added help

--- src/decode_bbf.cpp
+++ src/decode_bbf.cpp
@@ -70,66 +70,73 @@ namespace disposer_module::decode_bbf{
 			"rgba64f"
 		}};
 
+	constexpr auto dim1_types = dimension_c<
+			bool,
+			std::int8_t,
+			std::int16_t,
+			std::int32_t,
+			std::int64_t,
+			std::uint8_t,
+			std::uint16_t,
+			std::uint32_t,
+			std::uint64_t,
+			float,
+			double,
+			pixel::ga8,
+			pixel::ga16,
+			pixel::ga32,
+			pixel::ga64,
+			pixel::ga8u,
+			pixel::ga16u,
+			pixel::ga32u,
+			pixel::ga64u,
+			pixel::ga32f,
+			pixel::ga64f,
+			pixel::rgb8,
+			pixel::rgb16,
+			pixel::rgb32,
+			pixel::rgb64,
+			pixel::rgb8u,
+			pixel::rgb16u,
+			pixel::rgb32u,
+			pixel::rgb64u,
+			pixel::rgb32f,
+			pixel::rgb64f,
+			pixel::rgba8,
+			pixel::rgba16,
+			pixel::rgba32,
+			pixel::rgba64,
+			pixel::rgba8u,
+			pixel::rgba16u,
+			pixel::rgba32u,
+			pixel::rgba64u,
+			pixel::rgba32f,
+			pixel::rgba64f
+		>;
+
+	std::string format_description(){
+		std::ostringstream os;
+		std::size_t i = 0;
+		hana::for_each(dim1_types.types, [&os, &i](auto t){
+				os << "\n* " << list[i] << " => "
+					<< ct_pretty_name< typename decltype(t)::type >();
+				++i;
+			});
+		return os.str();
+	}
+
 	void init(std::string const& name, module_declarant& disposer){
 		auto init = generate_module(
-// 			description(
-// 				"Decodes an image in BBF format.\n\n" +
-// 				std::string(bmp::bbf_specification)
-// 			),
+			"Decodes an image in BBF format.\n\n"
+				/*+ std::string(bmp::bbf_specification)*/,
 			dimension_list{
-				dimension_c<
-					bool,
-					std::int8_t,
-					std::int16_t,
-					std::int32_t,
-					std::int64_t,
-					std::uint8_t,
-					std::uint16_t,
-					std::uint32_t,
-					std::uint64_t,
-					float,
-					double,
-					pixel::ga8,
-					pixel::ga16,
-					pixel::ga32,
-					pixel::ga64,
-					pixel::ga8u,
-					pixel::ga16u,
-					pixel::ga32u,
-					pixel::ga64u,
-					pixel::ga32f,
-					pixel::ga64f,
-					pixel::rgb8,
-					pixel::rgb16,
-					pixel::rgb32,
-					pixel::rgb64,
-					pixel::rgb8u,
-					pixel::rgb16u,
-					pixel::rgb32u,
-					pixel::rgb64u,
-					pixel::rgb32f,
-					pixel::rgb64f,
-					pixel::rgba8,
-					pixel::rgba16,
-					pixel::rgba32,
-					pixel::rgba64,
-					pixel::rgba8u,
-					pixel::rgba16u,
-					pixel::rgba32u,
-					pixel::rgba64u,
-					pixel::rgba32f,
-					pixel::rgba64f
-				>
+				dim1_types
 			},
 			module_configure(
-				make("data"_in, free_type_c< std::string >
-// 					, description("the BBF encoded image")
-				),
+				make("data"_in, free_type_c< std::string >,
+					"the BBF encoded image"),
 				make("format"_param, free_type_c< std::size_t >,
-// 					description(
-// 						"type of the BBF image\npossible values: " +
-// 						io_tools::range_to_string(list)
-// 					),
+					"set dimension 1 by value: " + format_description(),
 					parser_fn([](
 						auto const /*module*/,
 						std::string_view data,
@@ -148,9 +155,8 @@ namespace disposer_module::decode_bbf{
 					std::size_t const number = module("format"_param);
 					return solved_dimensions{index_component< 0 >{number}};
 				}),
-				make("image"_out, wrapped_type_ref_c< bitmap, 0 >
-// 					, description("the decoded image")
-				)
+				make("image"_out, wrapped_type_ref_c< bitmap, 0 >,
+					"the decoded image")
 			),
 			exec_fn([](auto module){
 				using type = typename

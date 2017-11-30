1e58546ee26b7f12e35f460652cebeb229992035
Benjamin Roland Buch <benni.buch@gmail.com>
2017-11-30 15:43:09 +0100

    better help text

--- src/transform_bitmap.cpp
+++ src/transform_bitmap.cpp
@@ -11,6 +11,7 @@
 #include <bitmap/rect_transform.hpp>
 #include <bitmap/rect_io.hpp>
 
+#include <io_tools/range_to_string.hpp>
 #include <io_tools/std_array_io.hpp>
 
 #include <boost/dll.hpp>
@@ -47,106 +48,121 @@ namespace disposer_module::transform_bitmap{
 		bmp::rect< long, long, std::size_t, std::size_t > const contour;
 	};
 
+	constexpr auto dim1 = dimension_c<
+			std::int8_t,
+			std::int16_t,
+			std::int32_t,
+			std::int64_t,
+			std::uint8_t,
+			std::uint16_t,
+			std::uint32_t,
+			std::uint64_t,
+			float,
+			double,
+			pixel::ga8,
+			pixel::ga16,
+			pixel::ga32,
+			pixel::ga64,
+			pixel::ga8u,
+			pixel::ga16u,
+			pixel::ga32u,
+			pixel::ga64u,
+			pixel::ga32f,
+			pixel::ga64f,
+			pixel::rgb8,
+			pixel::rgb16,
+			pixel::rgb32,
+			pixel::rgb64,
+			pixel::rgb8u,
+			pixel::rgb16u,
+			pixel::rgb32u,
+			pixel::rgb64u,
+			pixel::rgb32f,
+			pixel::rgb64f,
+			pixel::rgba8,
+			pixel::rgba16,
+			pixel::rgba32,
+			pixel::rgba64,
+			pixel::rgba8u,
+			pixel::rgba16u,
+			pixel::rgba32u,
+			pixel::rgba64u,
+			pixel::rgba32f,
+			pixel::rgba64f
+		>;
+
+	constexpr auto dim2 = dimension_c<
+			std::int8_t,
+			std::int16_t,
+			std::int32_t,
+			std::int64_t,
+			std::uint8_t,
+			std::uint16_t,
+			std::uint32_t,
+			std::uint64_t,
+			float,
+			double
+		>;
+
+	constexpr std::array< std::string_view, 10 > list{{
+			"8s",
+			"16s",
+			"32s",
+			"64s",
+			"8u",
+			"16u",
+			"32u",
+			"64u",
+			"32f",
+			"64f"
+		}};
+
+	std::string format_description(){
+		std::ostringstream os;
+		std::size_t i = 0;
+		hana::for_each(dim1.types, [&os, &i](auto t){
+				os << "\n* " << list[i] << " => "
+					<< ct_pretty_name< typename decltype(t)::type >();
+				++i;
+			});
+		return os.str();
+	}
 
 
 	void init(std::string const& name, module_declarant& disposer){
 		auto init = generate_module(
+			"perspective transform by 4 free points and an target rect",
 			dimension_list{
-				dimension_c<
-					std::int8_t,
-					std::int16_t,
-					std::int32_t,
-					std::int64_t,
-					std::uint8_t,
-					std::uint16_t,
-					std::uint32_t,
-					std::uint64_t,
-					float,
-					double,
-					pixel::ga8,
-					pixel::ga16,
-					pixel::ga32,
-					pixel::ga64,
-					pixel::ga8u,
-					pixel::ga16u,
-					pixel::ga32u,
-					pixel::ga64u,
-					pixel::ga32f,
-					pixel::ga64f,
-					pixel::rgb8,
-					pixel::rgb16,
-					pixel::rgb32,
-					pixel::rgb64,
-					pixel::rgb8u,
-					pixel::rgb16u,
-					pixel::rgb32u,
-					pixel::rgb64u,
-					pixel::rgb32f,
-					pixel::rgb64f,
-					pixel::rgba8,
-					pixel::rgba16,
-					pixel::rgba32,
-					pixel::rgba64,
-					pixel::rgba8u,
-					pixel::rgba16u,
-					pixel::rgba32u,
-					pixel::rgba64u,
-					pixel::rgba32f,
-					pixel::rgba64f
-				>,
-				dimension_c<
-					std::int8_t,
-					std::int16_t,
-					std::int32_t,
-					std::int64_t,
-					std::uint8_t,
-					std::uint16_t,
-					std::uint32_t,
-					std::uint64_t,
-					float,
-					double
-				>
+				dim1,
+				dim2
 			},
 			module_configure(
 				make("source_size"_param,
-					free_type_c< bmp::size< std::size_t > >),
+					free_type_c< bmp::size< std::size_t > >,
+					"expected size of the bitmaps"),
 				make("target_size"_param,
-					free_type_c< bmp::size< std::size_t > >),
+					free_type_c< bmp::size< std::size_t > >,
+					"width and height of the rect in the target image"),
 				make("format"_param,
 					free_type_c< std::optional< std::size_t > >,
+					"set dimension 2 by dimension 1 (default) or by value:"
+						+ format_description(),
 					parser_fn([](
 						auto const /*module*/,
 						std::string_view data,
 						hana::basic_type< std::optional< std::size_t > >
 					){
-						constexpr std::array< std::string_view, 10 > list{{
-								"8s",
-								"16s",
-								"32s",
-								"64s",
-								"8u",
-								"16u",
-								"32u",
-								"64u",
-								"32f",
-								"64f"
-							}};
 						auto iter = std::find(list.begin(), list.end(), data);
 						if(iter == list.end()){
-							std::ostringstream os;
-							os << "unknown value '" << data
-								<< "', valid values are: ";
-							bool first = true;
-							for(auto name: list){
-								if(first){ first = false; }else{ os << ", "; }
-								os << name;
-							}
-							throw std::runtime_error(os.str());
+							throw std::runtime_error("unknown value '"
+								+ std::string(data)
+								+ "', valid values are: "
+								+ io_tools::range_to_string(list));
 						}
-						return std::optional< std::size_t >
-							(iter - list.begin());
+						return iter - list.begin();
 					})),
-				make("image"_in, wrapped_type_ref_c< bitmap, 0 >),
+				make("image"_in, wrapped_type_ref_c< bitmap, 0 >,
+					"original bitmap"),
 				set_dimension_fn([](auto const module){
 					auto const optional_number = module("format"_param);
 					std::size_t const number =
@@ -169,6 +185,12 @@ namespace disposer_module::transform_bitmap{
 				}),
 				make("source_points"_param,
 					wrapped_type_ref_c< points_type, 1 >,
+					"4 points in the original bitmap that are mapped to the "
+					"rect points in the target bitmap\n"
+					"* p1 => top left point\n"
+					"* p2 => top right point\n"
+					"* p3 => bottom left point\n"
+					"* p4 => bottom right point",
 					parser_fn([](
 						auto const /*module*/,
 						std::string_view data,
@@ -182,7 +204,8 @@ namespace disposer_module::transform_bitmap{
 						return result;
 					})),
 				make("image"_out,
-					wrapped_type_ref_c< transformed_bitmap, 0, 1 >)
+					wrapped_type_ref_c< transformed_bitmap, 0, 1 >,
+					"transformed bitmap")
 			),
 			module_init_fn([](auto module){
 				auto t = module.dimension(hana::size_c< 1 >);
@@ -222,7 +245,12 @@ namespace disposer_module::transform_bitmap{
 					source_pixel_type, target_channel_type >;
 				using matrix_type = calc_type< target_channel_type >;
 				auto const& state = module.state();
+				auto const source_size = module("source_size"_param);
 				for(auto const& img: module("image"_in).references()){
+					if(img.size() != source_size){
+						throw std::logic_error("image has not expected size");
+					}
+
 					module("image"_out).push(bmp::transform_bitmap<
 							target_pixel_type, source_pixel_type, matrix_type
 						>(state.homography, img, state.contour));

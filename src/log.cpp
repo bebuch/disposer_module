4ed516352a5570ff2bb2b3328526ce999d8709b5
Benjamin Roland Buch <benni.buch@gmail.com>
2018-01-29 11:03:50 +0100

    fix head comment

--- src/log.cpp
+++ src/log.cpp
@@ -1,7 +1,7 @@
 //-----------------------------------------------------------------------------
 // Copyright (c) 2015-2018 Benjamin Buch
 //
-// https://github.com/bebuch/logsys
+// https://github.com/bebuch/disposer_module
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
